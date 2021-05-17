467bbc5e65ec93586e47d341fb83c1fd6375373d
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-05-17 14:59:28 +0900

    lesson8 配列を使う

--- js/main.js
+++ js/main.js
@@ -4,19 +4,21 @@
     const button = document.getElementById('btn');
 
     btn.addEventListener('click', () => {
-        const n = Math.floor(Math.random() * 3 );
-        btn.textContent = n;
-
-        switch (n) {
-            case 0:
-                btn.textContent = '大吉';
-                break;
-            case 1:
-                btn.textContent = '小吉';
-                break;
-            case 2:
-                btn.textContent = '凶';
-                break;
-        }
+        const results = ['大吉', '小吉', '凶'];
+        // const n = Math.floor(Math.random() * results.length);
+        // btn.textContent = results[n];
+        btn.textContent = results[Math.floor(Math.random() * results.length)];
+        
+        // switch (n) {
+        //     case 0:
+        //         btn.textContent = '大吉';
+        //         break;
+        //     case 1:
+        //         btn.textContent = '小吉';
+        //         break;
+        //     case 2:
+        //         btn.textContent = '凶';
+        //         break;
+        // }
     });
 }
