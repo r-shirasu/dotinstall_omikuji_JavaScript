83ca7f982d838be34ede7bead04ee4e8f8a31bd1
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-05-17 15:03:59 +0900

    lesson9 確率を操作する

--- js/main.js
+++ js/main.js
@@ -4,21 +4,18 @@
     const button = document.getElementById('btn');
 
     btn.addEventListener('click', () => {
-        const results = ['大吉', '小吉', '凶'];
-        // const n = Math.floor(Math.random() * results.length);
-        // btn.textContent = results[n];
-        btn.textContent = results[Math.floor(Math.random() * results.length)];
-        
-        // switch (n) {
-        //     case 0:
-        //         btn.textContent = '大吉';
-        //         break;
-        //     case 1:
-        //         btn.textContent = '小吉';
-        //         break;
-        //     case 2:
-        //         btn.textContent = '凶';
-        //         break;
-        // }
+        // const results = ['大吉', '小吉', '凶'];
+        // const results = ['大吉', '大吉', '大吉', '凶'];
+        // btn.textContent = results[Math.floor(Math.random() * results.length)];
+        const n = Math.random();
+        if (n < 0.05) {
+            btn.textContent = '大吉';　// 5%
+            
+        } else if (n < 0.2) {
+            btn.textContent = '中吉'; // 15% 
+        }　else {
+            btn.textContent = '凶';  // 残り80%
+        }
+
     });
 }
